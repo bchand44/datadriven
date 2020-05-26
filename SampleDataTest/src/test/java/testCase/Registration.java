3309c91aaba59b5efb5ddecdaf221cc56fc52968
birendra <birendra@10.0.0.182>
2020-05-26 17:54:53 -0400

    added excelreader class under utility, used data provider on registration class

--- SampleDataTest/src/test/java/testCase/Registration.java
+++ SampleDataTest/src/test/java/testCase/Registration.java
@@ -1,19 +1,52 @@
 package testCase;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
+import java.util.Hashtable;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 import base.TestBase;
 
 public class Registration extends TestBase {
 
-@Test 
+@Test(dataProvider="getData")
 
-public void registerUser()
+public void registerUser(Hashtable<String,String> data)
 {
 	System.out.println("Starting Registration");
-	log.debug("registration completed");
+	log.debug("registration started");
+	driver.findElement(By.xpath(or.getProperty("clickSignIn"))).click();
+	driver.findElement(By.id(or.getProperty("emailAddress"))).sendKeys(data.get("emailAddress"));
+	driver.findElement(By.name(or.getProperty("createAccount"))).click();
 }
 
+@DataProvider 
+
+public Object[][] getData()
+{
+	
+	String sheetName="Registration";
+	int rows = excel.getRowCount(sheetName);
+	int cols = excel.getColumnCount(sheetName);
+
+	Object[][] data = new Object[rows - 1][1];
+	
+	Hashtable<String,String> table = null;
+
+	for (int rowNum = 2; rowNum <= rows; rowNum++) { // 2
+
+		table = new Hashtable<String,String>();
+		
+		for (int colNum = 0; colNum < cols; colNum++) {
+
+			// data[0][0]
+			table.put(excel.getCellData(sheetName, colNum, 1), excel.getCellData(sheetName, colNum, rowNum));
+			data[rowNum - 2][0] = table;
+		}
+
+	}
+
+	return data;
+}
 
 }
