11fe8353ec44ab43726953281c2f72e729687693
birendra <birendra@10.0.0.182>
2020-05-26 01:42:02 -0400

    pushing project structure, does not have much code, just intial test base and some sample tests

---
+++ SampleDataTest/src/test/java/testCase/Registration.java
@@ -0,0 +1,19 @@
+package testCase;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+import base.TestBase;
+
+public class Registration extends TestBase {
+
+@Test 
+
+public void registerUser()
+{
+	System.out.println("Starting Registration");
+	log.debug("registration completed");
+}
+
+
+}
